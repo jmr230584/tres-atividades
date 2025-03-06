883c3b1fe26583a548ff69adaa711a98a7b60b58
José Mateus <mateusjoseph@gmail.com>
2025-03-06 13:33:10 -0300

    respondendo atividade 2

--- atividade1.ts
+++ atividade1.ts
@@ -1,13 +1,13 @@
 let teclado = require (`prompt-sync`)();
-let total_pessoas: number = 0;
+let total_pessoas: number = 1;
 let cont_condicao_pessoas: number = 0;
 
 do{
     let idade: number = 0;
     let salario: number = 0;
     
-    idade = parseInt(teclado(`Qual a idade: `));
-    salario = parseInt(teclado(`Qual o salario: `));
+    idade = parseInt(teclado(`Qual a idade do funcionário ${total_pessoas}: `));
+    salario = parseInt(teclado(`Qual o salario do funcionário ${total_pessoas}: `));
 
     if (idade < 30 && salario > 3000){
         cont_condicao_pessoas++;
@@ -16,6 +16,6 @@ do{
     else{
         total_pessoas++;
     }
-}while(total_pessoas <= 50);
+}while(total_pessoas <= 5);
 
 console.log(`O total de pessoas que atendem a cndição de ser menor que 30 anos e ganhar mais de R$3.000,00 é ${cont_condicao_pessoas}`)
---
+++ atividade2.ts
@@ -0,0 +1,8 @@
+let x: number = 0; 
+
+do{
+    console.log(x);
+    x++;
+}while(x < 11);
+
+console.log(`Fim de programa!`)
